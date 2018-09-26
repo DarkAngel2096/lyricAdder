3bdba5949ff8dbf75c043ae5da4eef2b73e367ad
DarkAngel2096 <toni.nordstrom@hotmail.com>
2018-09-26 13:23:15 +0300

    Super simple Electron project setup
    got a super simple Electron app working, has a html doc and a base .js file

---
+++ main.js
@@ -0,0 +1,30 @@
+const electron = require("electron");
+const { app, BrowserWindow } = require("electron");
+
+let win;
+
+function createWindow () {
+    win = new BrowserWindow({ width: 800, height: 600});
+
+    win.loadFile("index.html");
+
+    win.webContents.openDevTools();
+
+    win.on("closed", () => {
+        win = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (win === null) {
+        createWindow();
+    }
+});
