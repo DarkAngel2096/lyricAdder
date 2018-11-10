6a7e932e0569cc605c8600b721cbf50c4c38257b
DarkAngel2096 <toni.nordstrom@hotmail.com>
2018-11-10 14:38:11 +0200

    v.0.2.2.1
    test 2 on the chart scanner, now only takes charts needed, shouldn't hit the V8 String limit anymore

--- dataGather.js
+++ dataGather.js
@@ -15,13 +15,13 @@ function mainGather(pathToSongs, pathToBackup) {
     pathToBackupFolder = pathToBackup;
 
     gatherChartPaths();
-
 }
 
 // function to gather paths to all chart files
 function gatherChartPaths() {
     chartPathArray = [];
     allChartInfoArray = [];
+    allChartArray = [];
 
     console.log(pathToSongsFolder.length);
 
@@ -35,16 +35,80 @@ function gatherChartPaths() {
     for (var i = 0; i < chartPathArray.length; i++) {
         var chartInfo = fs.readFileSync(chartPathArray[i], "utf8");
 
+        var chartSplit = chartInfo.trim().split("\r\n");
+
+        var indicies = [];
+        var element = "{";
+        var idx = chartSplit.indexOf(element);
+
+        while (idx != -1) {
+            indicies.push(idx);
+            idx = chartSplit.indexOf(element, idx + 1);
+        }
+
+        var countSingle = 0;
+        var countDoubleGuitar = 0;
+        var countDoubleBass = 0;
+        var countDoubleRythm = 0;
+        var countDrums = 0;
+        var countKeyboards = 0;
+        var countGHLGuitar = 0;
+        var countGHLBass = 0;
+
+        for (var j = 0; j < indicies.length; j++) {
+            var currentTag = chartSplit[indicies[j] - 1];
+
+            if (currentTag != "[Song]" && currentTag != "[SyncTrack]" && currentTag != "[Events]") {
+                //console.log("found: " + currentTag);
+
+                var tempTag = currentTag.substring(1, currentTag.length -1);
+
+                if (tempTag.endsWith("Single")) {
+                    countSingle++;
+                } else if (tempTag.endsWith("DoubleGuitar")) {
+                    countDoubleGuitar++;
+                } else if (tempTag.endsWith("DoubleBass")) {
+                    countDoubleBass++;
+                } else if (tempTag.endsWith("DoubleRythm")) {
+                    countDoubleRythm++;
+                } else if (tempTag.endsWith("Drums")) {
+                    countDrums++;
+                } else if (tempTag.endsWith("Keyboard")) {
+                    countKeyboards++;
+                } else if (tempTag.endsWith("GHLGuitar")) {
+                    countGHLGuitar++;
+                } else if (tempTag.endsWith("GHLBass")) {
+                    countGHLBass++;
+                }
+
+                //console.log(countSingle + ", " + countDoubleGuitar + ", " + countDoubleBass + ", " + countDoubleRythm + ", " + countDrums + ", " + countKeyboards + ", " + countGHLGuitar + ", " + countGHLBass);
+            }
+        }
+
+        if (countSingle > 1 || countDoubleGuitar > 1 || countDoubleBass > 1 || countDoubleRythm > 1 || countDrums > 1 || countKeyboards > 1 || countGHLGuitar > 1 || countGHLBass > 1) {
+            console.log("Chart number: " + i + " is put into the output");
+
+            allChartInfoArray.push(chartInfo);
+        } else {
+            console.log("Chart number: " + i + " is removed");
+        }
+
         console.log("chart number: " + i + ", is: " + chartInfo.length + " characters long");
         totalLength += chartInfo.length;
 
-        allChartInfoArray.push(chartInfo);
+        allChartArray.push(chartInfo);
     }
 
-    console.log("Total length of all charts combined is: " + totalLength);
+    console.log("Total amount of .chart files found: " + allChartArray.length + " and the total length is: " + totalLength);
+
+    var outputLength = 0;
+
+    for (var i = 0; i < allChartInfoArray.length; i++) {
+        outputLength += allChartInfoArray[i].length;
+    }
 
     //console.log(allChartInfoArray);
-    console.log("Total amount of .chart files found: " + allChartInfoArray.length);
+    console.log("Total amount of .chart files in the output: " + allChartInfoArray.length + " and their length is: " + outputLength);
 
     try {
         fs.writeFileSync(pathToBackupFolder + "\\allChartInfo.json", allChartInfoArray.join("\r\n"), "utf8");
