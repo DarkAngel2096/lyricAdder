5398b5afc9eacc78072fccd5c729cd64bcdeb23b
DarkAngel2096 <toni.nordstrom@hotmail.com>
2019-03-08 11:33:47 +0200

    latest newChart.js file changes
    nothing much, commented out code mainly

--- mainJS/newChart.js
+++ mainJS/newChart.js
@@ -466,15 +466,71 @@ function createObjects(infoArr) {
 
 					console.log(lineInfo);
 
+					if (lineInfo[0] == chordTick) {
+						// if the line has the same tick as the one before
+						console.log("chord found: " + lineInfo + ", tick: " + chordTick);
 
-					/*
-					lineInfo[2] = parseInt(lineInfo[2]);
+						chordArray.push(lineInfo);
+					} else {
+						// if not, go through what's found and make objects, also add the first one to the list
+						for (let j = 0; j < 2; j++) {
+							for (let k = 0; k < chordArray.length; k++) {
+								switch (j) {
+									case 1: {
+										switch (chordArray[k][2]) {
+											case 5: toForce = true; break;
+											case 6: toTap = true; break;
+											case 0:
+											case 1:
+											case 2:
+											case 3:
+											case 4:
+											case 7:
+											case 8: {
+												numberOfNotes++;
+											}
+										}
+										break;
+									}
+									case 2: {
+										switch (chordArray[k][2]) {
+											case 0:
+											case 1:
+											case 2:
+											case 3:
+											case 4:
+											case 7:
+											case 8: {
+												if (numberOfNotes > 1) {
+													returnArray.push(new Note(lineInfo[0], lineInfo[2], toForce, toTap, true, parseInt(lineInfo[3])))
+												} else {
+													returnArray.push(new Note(lineInfo[0], lineInfo[2], toForce, toTap, false, parseInt(lineInfo[3])))
+												}
+												break;
+											}
+										}
+										break;
+									}
+								}
+							}
+						}
+
+						chordArray = [];
+						chordArray.push(lineInfo);
+						chordTick = lineInfo[0];
+						numberOfNotes = 0;
+					}
+
+
+							// not working either...
+/*					lineInfo[2] = parseInt(lineInfo[2]);
 
 					// check if it's part of a bunch of events with the same tick (also taking taps and forces)
-					if (lineInfo[2] == chordTick) {
+					if (lineInfo[0] == chordTick) {
 						//console.log("pushed: " + lineInfo + " to chord array");
 						chordArray.push(lineInfo);
-					} else if (lineInfo[2] != chordTick) {
+						numberOfNotes++;
+					} else if (lineInfo[0] != chordTick) {
 						if (chordArray.length > 1) {
 							for (var j = 0; j < 2; j++) {
 								for (var k = 0; k < chordArray.length; k++) {
@@ -532,6 +588,7 @@ function createObjects(infoArr) {
 							chordArray = [];
 						} else {
 							returnArray.push(new Note(lineInfo[0], lineInfo[2], false, false, false, parseInt(lineInfo[3])));
+							numberOfNotes++;
 						}
 
 						chordArray.push(lineInfo);
